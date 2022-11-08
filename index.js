c79da90f5891882719bc96eec290916ec6ea4641
NathanMad <nathan@internit.com.br>
2022-11-08 12:48:27 -0300

    otimizando js + deletando img

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
         btn.addEventListener('click', (e) => {
             if(document.querySelector(btn.dataset.target).dataset.state == 'false') {
                 document.querySelector(btn.dataset.target).dataset.state = 'true';
-                if( btn.children[0].children[0].classList.contains('fa-caret-down')) {
-                    btn.children[0].children[0].classList.remove('fa-caret-down');
-                    btn.children[0].children[0].classList.add('fa-caret-up');
+                if( btn.querySelector('.fa-caret-down')) {
+                    btn.querySelector('.fa-caret-down').classList.add('fa-caret-up');
+                    btn.querySelector('.fa-caret-down').classList.remove('fa-caret-down');
                 }
             } else {
                 document.querySelector(btn.dataset.target).dataset.state = 'false';
-                if( btn.children[0].children[0].classList.contains('fa-caret-up')) {
-                    btn.children[0].children[0].classList.remove('fa-caret-up');
-                    btn.children[0].children[0].classList.add('fa-caret-down');
+                if( btn.querySelector('.fa-caret-up')) {
+                    btn.querySelector('.fa-caret-up').classList.add('fa-caret-down');
+                    btn.querySelector('.fa-caret-up').classList.remove('fa-caret-up');
                 }
             }
         });
